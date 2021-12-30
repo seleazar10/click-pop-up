f0d0e4a1defe3c79959f1a5e430180d81bcba4b8
selea <sve5040@gmail.com>
2021-12-30 18:35:53 -0500

    flies set up and btn-one works

---
+++ script.js
@@ -0,0 +1,7 @@
+let btnOne = document.querySelector('.btn-one');
+let btnTwo = document.querySelector('.btn-two');
+let btnThree = document.querySelector('btn-three');
+
+btnOne.addEventListener('click', function () {
+	console.log('btn one is clicked');
+});
