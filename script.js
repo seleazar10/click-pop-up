c773ada223695b244ee7ddb354ad072e6bd14ea9
selea <sve5040@gmail.com>
2022-01-03 20:55:32 -0600

    able to close and open all tabs - dyno

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
-let btnOne = document.querySelector('.btn-one');
-let btnTwo = document.querySelector('.btn-two');
-let btnThree = document.querySelector('btn-three');
+let navBtnPopUpArr = document.querySelectorAll('.nav-btn');
+let popUpCloseBtnArr = document.querySelectorAll('.pop-x-btn ');
+let popUpToClose;
+let currIDClicked;
 
-btnOne.addEventListener('click', function () {
-	console.log('btn one is clicked');
+//display pop up window
+for (let i = 0; i < navBtnPopUpArr.length; i++) {
+	navBtnPopUpArr[i].addEventListener('click', function () {
+		displayPopUp(navBtnPopUpArr[i]);
+	});
+}
+//close pop up window
+for (let i = 0; i < popUpCloseBtnArr.length; i++) {
+	popUpCloseBtnArr[i].addEventListener('click', function () {
+		closePopUpWindFunc(popUpCloseBtnArr[i]);
+	});
+}
+
+let displayPopUp = function (popUpId) {
+	currIDClicked = `pop-up-${popUpId.id}`;
+	popUpToClose = document.getElementById(currIDClicked);
+	popUpToClose.classList.remove('hidden');
+
+	console.log(`current id ${currIDClicked}`, popUpToClose);
+};
+
+let closePopUpWindFunc = function (popUpCloseId) {
+	let btnClsdId = popUpCloseId.id;
+	currIDClicked = `pop-up-${btnClsdId.slice(8)}`;
+	popUpToClose = document.getElementById(currIDClicked);
+	popUpToClose.classList.add('hidden');
+
+	console.log(`current id ${currIDClicked}`, popUpToClose);
+};
+
+let closeUpOnDOM = document.addEventListener('keydown', function (e) {
+	console.log(e);
 });
